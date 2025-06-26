dd7e9945f393394eb0f395a1a9b0e7436ded56c6
Eric Nohara-LeClair <Eric.NoharaLeClair@dotfoods.com>
2025-06-26 14:41:25 -0500

    Authorization header fix

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,15 +1,12 @@
 import os
-import json
 import requests
 
 NEXT_PUBLIC_PORTFOLIO_API_URL = os.environ['NEXT_PUBLIC_PORTFOLIO_API_URL']
 PORTFOLIO_PRIVATE_API_KEY = os.environ['PORTFOLIO_PRIVATE_API_KEY']
 USER_EMAIL = os.environ['USER_EMAIL']
 
-api_key = f"Bearer ${PORTFOLIO_PRIVATE_API_KEY}"
-
 headers = {
-    "Authorization": api_key,
+    "Authorization": f"Bearer {PORTFOLIO_PRIVATE_API_KEY}",
     "User-Email": USER_EMAIL
 }
 
