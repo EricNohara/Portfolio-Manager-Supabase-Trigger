c720f1932e4c9e7592d387e53cdca469142eace1
Eric Nohara-LeClair <Eric.NoharaLeClair@dotfoods.com>
2025-06-26 14:18:46 -0500

    Add request to portfolio manager

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,7 +1,17 @@
+import os
 import json
+import requests
+
+NEXT_PUBLIC_PORTFOLIO_API_URL = os.environ['NEXT_PUBLIC_PORTFOLIO_API_URL']
+PORTFOLIO_PRIVATE_API_KEY = os.environ['PORTFOLIO_PRIVATE_API_KEY']
+USER_EMAIL = os.environ['USER_EMAIL']
+
+api_key = f"Bearer ${PORTFOLIO_PRIVATE_API_KEY}"
+
+headers = {
+    "Authorization": api_key,
+    "User-Email": USER_EMAIL
+}
 
 def lambda_handler(event, context):
-    return  {
-        'statusCode': 200,
-        'body': json.dumps('Hello from lambda and GitHub actions!')
-    }
+    return requests.get(NEXT_PUBLIC_PORTFOLIO_API_URL, headers=headers)
